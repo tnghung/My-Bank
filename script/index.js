018f040c8fe564ce1ab197286849896062a07e93
tnghung <tnghung2812@gmail.com>
2022-05-13 16:58:38 +0700

    Make hover navigate animation by js

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@ const section1 = document.querySelector('#section--1');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const nav = document.querySelector('.nav');
 
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
@@ -24,6 +25,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+// Handle hover for navigation
+
 btnsOpenModal.forEach((btnOpenModal) => btnOpenModal.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
@@ -37,6 +40,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Tabbed Components
 containerTab.addEventListener('click', function (e) {
   // Defined tab's button target
   const tabClicked = e.target.closest('.operations__tab');
@@ -52,3 +56,30 @@ containerTab.addEventListener('click', function (e) {
   // Add active class for label depend on dataset of button
   document.querySelector(`.operations__content--${tabClicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+const handleHover = (opacity) =>
+  function (e) {
+    if (e.target.classList.contains('nav__link')) {
+      const linkHover = e.target;
+
+      // Get all nav__link elements
+      const siblings = linkHover.closest('.nav__links').querySelectorAll(`.nav__link`);
+      console.log(siblings);
+      // Get logo element
+      const logo = linkHover.closest('.nav').querySelector('.nav__logo');
+
+      // Hover link will change opacity others
+      siblings.forEach((link) => {
+        if (link !== linkHover) {
+          link.style.opacity = opacity;
+        }
+      });
+
+      // Make logo change opacity
+      logo.style.opacity = opacity;
+    }
+  };
+
+// Nav hover
+nav.addEventListener('mouseover', handleHover(0.5));
+nav.addEventListener('mouseout', handleHover(1));
