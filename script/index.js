5cc47d8646932da1d7b7c0f0927afa9a625dcc86
tnghung <tnghung2812@gmail.com>
2022-05-13 15:49:08 +0700

    Finish tabbed components

--- script/index.js
+++ script/index.js
@@ -1,9 +1,17 @@
 'use strict';
+const section1 = document.querySelector('#section--1');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnsTab = document.querySelectorAll('.operations__tab');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const containerTab = document.querySelector('.operations__tab-container');
+
+const labelsTab = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -21,16 +29,26 @@ btnsOpenModal.forEach((btnOpenModal) => btnOpenModal.addEventListener('click', o
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+btnScrollTo.addEventListener('click', () => section1.scrollIntoView({ behavior: 'smooth' }));
+
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+containerTab.addEventListener('click', function (e) {
+  // Defined tab's button target
+  const tabClicked = e.target.closest('.operations__tab');
+  if (!tabClicked) return;
 
-btnScrollTo.addEventListener('click', () => section1.scrollIntoView({ behavior: 'smooth' }));
+  // Remove all active button tabs and content tabs
+  btnsTab.forEach((btn) => btn.classList.remove('operations__tab--active'));
+  labelsTab.forEach((label) => label.classList.remove('operations__content--active'));
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+  // Add active class for button clicked
+  tabClicked.classList.add('operations__tab--active');
+
+  // Add active class for label depend on dataset of button
+  document.querySelector(`.operations__content--${tabClicked.dataset.tab}`).classList.add('operations__content--active');
+});
