a120e5d526979a42399294b7b62c41854a2e9355
tnghung <tnghung2812@gmail.com>
2022-05-14 12:52:02 +0700

    Use IntersectionObserver API  to make sticky nav

--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
@@ -11,6 +12,7 @@ const btnsTab = document.querySelectorAll('.operations__tab');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const containerTab = document.querySelector('.operations__tab-container');
+const containerNav = document.querySelector('.nav__links');
 
 const labelsTab = document.querySelectorAll('.operations__content');
 
@@ -57,6 +59,7 @@ containerTab.addEventListener('click', function (e) {
   document.querySelector(`.operations__content--${tabClicked.dataset.tab}`).classList.add('operations__content--active');
 });
 
+// Nav hover
 const handleHover = (opacity) =>
   function (e) {
     if (e.target.classList.contains('nav__link')) {
@@ -64,7 +67,7 @@ const handleHover = (opacity) =>
 
       // Get all nav__link elements
       const siblings = linkHover.closest('.nav__links').querySelectorAll(`.nav__link`);
-      console.log(siblings);
+
       // Get logo element
       const logo = linkHover.closest('.nav').querySelector('.nav__logo');
 
@@ -80,6 +83,50 @@ const handleHover = (opacity) =>
     }
   };
 
-// Nav hover
+// Hover in and out animation
 nav.addEventListener('mouseover', handleHover(0.5));
 nav.addEventListener('mouseout', handleHover(1));
+
+// Scroll smooth animation
+containerNav.addEventListener('click', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const linkClicked = e.target.closest('.nav__link');
+    const id = linkClicked.getAttribute('href');
+    if (id === '#') return;
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Make sticky nav
+// Cách 1: dùng window với event scroll
+// window.addEventListener('scroll', function () {
+//   const ysection1 = section1.getBoundingClientRect().y;
+//   // console.log(ysection1);
+//   if (ysection1 <= 0) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// });
+
+// Cách 2: dùng IntersectionObserver
+const heightNav = nav.getBoundingClientRect().height;
+const stickyNav = function (entries, observe) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    nav.classList.remove('sticky');
+  } else nav.classList.add('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  // Phần trăm này là phần trăm đối tượng được observe(ở đây là header) khi hiển thị trong thằng được chọn làm observer (ở đây là viewport do root = null)
+  // Ở đây header được viewport observe và giá trị threshold là 0
+  // nên khi header không hiển thị trong viewport (tức là viewport và header không intersecting) thì giá trị isIntersecting = true
+  threshold: 0,
+  // Giống như bên CSS, ở đây là dịch view port top: -90px
+  rootMargin: `-${heightNav}px`,
+});
+
+headerObserver.observe(header);
